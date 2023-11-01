d916983c27bdca01f1ea7099ff3a611e575ff009
vishwa-akshat <akvs0707@gmail.com>
2023-11-01 19:23:46 +0530

    Added navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,23 @@
+import "./navbar.scss";
+
+export default function Navbar() {
+    return (
+        <div className="navbar-container">
+            <p className="logo-text">XCELERATE FITNESS</p>
+            <div className="navbar-links-wrapper">
+                <a href="#" className="nav-link">
+                    Home
+                </a>
+                <a href="#" className="nav-link">
+                    Goals
+                </a>
+                <a href="#" className="nav-link">
+                    Services
+                </a>
+                <a href="#" className="nav-link">
+                    Contact
+                </a>
+            </div>
+        </div>
+    );
+}
