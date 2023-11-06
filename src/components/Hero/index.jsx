8068a95efeb52a49ead2829c1db16ec0b5b4becc
vishwa-akshat <akvs0707@gmail.com>
2023-11-06 20:22:07 +0530

    Hero section complete

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,5 +1,8 @@
 import BicepImage from "../../assets/bicep.svg";
 import AstrekImage from "../../assets/astrek.svg";
+import HeroImage1 from "../../assets/hero_img1.jpg";
+import HeroImage2 from "../../assets/hero_img2.jpg";
+import SmileyImage from "../../assets/smiley.svg";
 
 import "./hero.scss";
 
@@ -25,9 +28,39 @@ export default function Hero() {
                     <button className="free-pass-block">Free pass</button>
                 </div>
             </div>
-            <div className="image-small-wrapper"></div>
-            <div className="image-large-wrapper"></div>
-            <div className="info-wrapper"></div>
+            <div className="hero-part2-wrapper">
+                <div className="image-part1-wrapper">
+                    <div className="image-small-wrapper">
+                        <div className="overlay-text">
+                            Reach your body goals
+                        </div>
+                        <img
+                            src={HeroImage1}
+                            className="image-1"
+                            alt="exercise"
+                        />
+                    </div>
+                    <div className="info-wrapper">
+                        <div className="smiley-wrapper">
+                            <img
+                                src={SmileyImage}
+                                className="smiley"
+                                alt="smiley"
+                            />
+                        </div>
+                        <div className="customer-number">950k+</div>
+                        <div className="happy-customer">Happy customers</div>
+                    </div>
+                </div>
+                <div className="image-large-wrapper">
+                    <img src={HeroImage2} className="image-2" alt="exercise" />
+                    <div className="overlay-content">
+                        <p className="text-1">Lets try</p>
+                        <p className="text-2">Reach your body goals</p>
+                        <p className="text-3">body goals target</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
